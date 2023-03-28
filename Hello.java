f533e49399f1b3d8b1785e9eaee46a1cd5f87337
Yardenhoro <yarden218@gmail.com>
2023-03-29 01:02:07 +0300

    the file Hello.java was added

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 			System.out.println("another print !");
 			for(int i=1;i<=10;i++){
-					System.out.println("Hello world..."+i);
+					System.out.println("another print..."+i);
 		}
 	}
 }
